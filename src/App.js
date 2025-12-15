bc8de4b1fa2abe350cd77cea969c40f43468abc8
MINEZERI <144104450+MINEZERI@users.noreply.github.com>
2025-12-15 20:12:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Content from './components/Content';
 import Image from './components/Image';
 import GoodsGallery from './components/GoodsGallery';
-
+ 
 function App() {
 	return (
 		<div className="App">
